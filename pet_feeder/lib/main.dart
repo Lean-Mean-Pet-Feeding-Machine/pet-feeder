6916614cdd6dc77f8e400d1f2accca540c00e3f8
Silvia <silviadb48@gmail.com>
2023-09-26 18:10:45 -1000

    Created Sign-Up Page

--- pet_feeder/lib/main.dart
+++ pet_feeder/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'signup.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(
+    MaterialApp(
+      //I set the initial route to SignUpPage for testing purposes, feel free to change it to the log in page when ready
+      home: SignUpPage(), // Show SignUpPage initially
+      initialRoute: '/signup',
+      routes: {
+        '/signup': (context) => SignUpPage(), // Route for SignUpPage
+        '/myapp': (context) =>
+            MyApp(settingsController: settingsController), // Route for MyApp
+      },
+    ),
+  );
 }
---
+++ pet_feeder/lib/signup.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController =
+      TextEditingController(); // Separate controller for Confirm Password
+  final _emailController = TextEditingController();
+  final _zipCodeController =
+      TextEditingController(); // Separate controller for Zip Code
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                const SizedBox(height: 16.0),
+                const Text('Please register:'),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Confirm Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _zipCodeController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Zip Code',
+              ),
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _emailController.clear();
+                    _passwordController.clear();
+                    _confirmPasswordController.clear();
+                    _zipCodeController.clear();
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Simulate a sign-up process (replace with your actual sign-up logic)
+                    bool signUpSuccessful =
+                        true; // Replace with actual sign-up logic
+
+                    if (signUpSuccessful) {
+                      Navigator.pushReplacementNamed(context, '/myapp');
+                    }
+                  },
+                  child: const Text('SIGN UP'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
